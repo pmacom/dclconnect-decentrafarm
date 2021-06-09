4d84f86d29525701d50fa6279e113ddbcbd20b82
Patrick Macom <patrickmacom@gmail.com>
2021-06-09 19:53:45 -0400

    highlight looks good

--- src/items/boxHighlight.ts
+++ src/items/boxHighlight.ts
@@ -1,6 +1,4 @@
-import { HoldableEntity } from "src/components/holdable";
-import { InteractibleEntity } from "src/components/interactible";
-
+const boxHighlightTexture = new Texture("models/textures/striped_texture_colors3.png")
 @Component("BoxHighlightAnimation")
 export class BoxHighlightAnimation {
   constructor(public speed: number = 1) {}
@@ -8,13 +6,16 @@ export class BoxHighlightAnimation {
 
 const startUVs = [ 0,1, .5,1, .5,.5, 0,.5 ]
 const endUVs   = [.5,1, 1,1, 1,.5, .5,.5 ]
-const invisUVs = [ 0,0, 0,0, 0,0, 0,0 ]
-
+const invisUVs = [ 0.6, 0.4, 0.9, 0.4, 0.9, 0.1, 0.6, .01  ]
+const planeUVs = [ 0,.5, .5,.5, .5,0, 0,0 ]
+// const planeUVs = [ 0,.5, .5,.5, .5,0, 0,0 ]
 export class BoxHighlight extends Entity {
     public transform : Transform
     public boxShape: BoxShape
+    public frameEntity: Entity
+    public frameShape: PlaneShape
     public timer: number = 0
-    public duration: number = 2
+    public duration: number = 3
 
     constructor() {
         super()
@@ -23,16 +24,41 @@ export class BoxHighlight extends Entity {
             scale: new Vector3(1,1,1),
             rotation: new Quaternion().setEuler(0,0,0)
         })
+        this.frameEntity = new Entity()
         this.boxShape = new BoxShape()
+        this.frameShape = new PlaneShape()
         this.addComponent(this.boxShape)
         this.addComponent(new BoxHighlightAnimation())
 
-        const myMaterial = new Material()
-        let myTexture = new Texture("models/textures/striped_texture_colors.png", { hasAlpha: true })
-        myMaterial.albedoTexture = myTexture
-        myMaterial.alphaTexture = myTexture
-        this.addComponent(myMaterial)
+        const boxHighlightMaterial = new Material()
+        boxHighlightMaterial.roughness = 1
+        boxHighlightMaterial.emissiveColor = new Color3(0,0,1)
+        boxHighlightMaterial.albedoTexture = boxHighlightTexture
+        boxHighlightMaterial.alphaTexture = boxHighlightTexture
+        this.addComponent(boxHighlightMaterial)
+
+        this.addFrame()
         engine.addEntity(this)
+    }
+
+    addFrame() {
+      // planeUVs
+      this.frameEntity.addComponent(this.frameShape)
+      this.frameEntity.addComponentOrReplace(new Transform({
+        position: new Vector3(0, -.5, 0),
+        scale: new Vector3(1, 1, 1),
+        rotation: new Quaternion().setEuler(90,0,0)
+      }))
+      this.frameShape.uvs = [...planeUVs, ...planeUVs]
+
+      const frameMaterial = new Material()
+      frameMaterial.roughness = 1
+      frameMaterial.emissiveColor = new Color3(10,0,0)
+      frameMaterial.albedoTexture = boxHighlightTexture
+      frameMaterial.alphaTexture = boxHighlightTexture
+      this.frameEntity.addComponent(frameMaterial)
+
+      this.frameEntity.setParent(this)
     }
 
     updateUV(dt: number) {
